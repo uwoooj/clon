5ae3aaa6d37d2949e06cb6b9dff0c01439d9a7c3
uwoooj <101697316+uwoooj@users.noreply.github.com>
2023-07-26 09:39:05 +0900

    2
    2

--- 0725/js/mouse.js
+++
@@ -1,7 +0,0 @@
-let mouseCursor = document.getElementById("cursor");
-
-document.addEventListener("mousemove", cursor);
-function cursor(e) {
-  mouseCursor.style.top = e.pageY + document.body.scrollTop + "px";
-  mouseCursor.style.left = e.pageX + document.body.scrollLeft + "px";
-}
